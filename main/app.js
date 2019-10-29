b724a09ec7015561a12af286fa9019155edaf77c
Ubuntu <ubuntu@ip-172-31-32-102.us-east-2.compute.internal>
2019-10-29 21:34:40 +0000

    my homework1

--- main/app.js
+++ main/app.js
@@ -1,7 +1,54 @@
 function distance(first, second){
 	//TODO: implementați funcția
 	// TODO: implement the function
+    try{
+        if(!(first instanceof Array)) {
+            throw 'InvalidType'
+        }
+        if(!(second instanceof Array)){
+             throw 'InvalidType'
+        }
+   }
+    catch(err){
+	    console.warn(err.stack)
+    }
+
+   let i=0;
+   let len=0;
+   let len2=0;
+   let result=0;
+   if (first.length >=second.length){
+       len=second.length
+       len2=first.length
+
+   }
+   else{
+       len=first.length
+       len2=second.length
+   }
+   while (i<len-1){
+       if(first[i] === second[i]){
+           i++;
+       }
+    //   else if(first[i] !== second[i]){
+    //       result=result+1;
+    //       i++
+    //   }
+       else{
+           result=result+1;
+           i++
+       }
+   }
+   for(let j=len; j<len2;j++){
+       result=result+1;
+   }
+   return result;
 }
+
+
+		
+
+
 
 
 module.exports.distance = distance
