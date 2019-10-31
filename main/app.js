eba51540632ff39275ac09fdc582f5f22d9281dc
Ubuntu <ubuntu@ip-172-31-32-102.us-east-2.compute.internal>
2019-10-31 08:27:47 +0000

    my homework_1

--- main/app.js
+++ main/app.js
@@ -5,11 +5,16 @@ function distance(first, second){
             throw new Error("InvalidType") 
    }
 
-
    let i=0;
    let len=0;
    let len2=0;
    let result=0;
+   function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+   first=first.filter(onlyUnique);
+   second=second.filter(onlyUnique);
+   
    if (first.length >=second.length){
        len=second.length
        len2=first.length
@@ -19,15 +24,27 @@ function distance(first, second){
        len=first.length
        len2=second.length
    }
+   
+   if(first.length==0 && second.length==0)
+   {
+       return 0;
+   }
+   else
+   {
    while (i<len-1){
        if(first[i] === second[i]){
            i++;
+           
        }
-    //   else if(first[i] !== second[i]){
-    //       result=result+1;
-    //       i++
-    //   }
-       else{
+         
+      else if((String(first[i])==String(second[i]))&&(!(first[i] === second[i])))
+           {
+               result=result+1
+               i++
+           }
+       
+       else
+       {
            result=result+1;
            i++
        }
@@ -36,6 +53,7 @@ function distance(first, second){
        result=result+1;
    }
    return result;
+   }
 }
 
 
