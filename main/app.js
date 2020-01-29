fec8cb4fc38d7862aaa267dbfd0439089ef51a7f
Ubuntu <ubuntu@ip-172-31-32-102.us-east-2.compute.internal>
2020-01-29 10:34:18 +0000

    homework1

--- main/app.js
+++ main/app.js
@@ -1,59 +1,28 @@
 function distance(first, second){
 	//TODO: implementați funcția
 	// TODO: implement the function
-        if(!( Array.isArray(first)) ||!( Array.isArray(second)) ) {
-            throw new Error("InvalidType") 
-   }
-
-   let i=0;
-   let len=0;
-   let len2=0;
-   let result=0;
-   function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-}
-   first=first.filter(onlyUnique);
-   second=second.filter(onlyUnique);
-   
-   if (first.length >=second.length){
-       len=second.length
-       len2=first.length
-
-   }
-   else{
-       len=first.length
-       len2=second.length
-   }
-   
-   if(first.length==0 && second.length==0)
-   {
-       return 0;
-   }
-   else
-   {
-   while (i<len-1){
-       if(first[i] === second[i]){
-           i++;
-           
-       }
-         
-      else if((String(first[i])==String(second[i]))&&(!(first[i] === second[i])))
-           {
-               result=result+1
-               i++
-           }
-       
-       else
-       {
-           result=result+1;
-           i++
-       }
-   }
-   for(let j=len; j<len2;j++){
-       result=result+1;
-   }
-   return result;
-   }
+	if(!( Array.isArray(first)) ||!( Array.isArray(second))){
+		throw new Error("InvalidType");
+	}else{
+		var unique1 = first.filter((v, i, a) => a.indexOf(v) === i); 
+		var unique2 = second.filter((v, i, a) => a.indexOf(v) === i);
+		let diff=0;
+		let lMax=0;
+		if(unique1.length>unique2.length){
+			lMax=unique1.length;
+		}else{
+			lMax=unique2.length;
+		}
+		for(let i=0;i<=lMax;i++){
+			if((!(unique1[i]===unique2[i]))){
+				diff++;
+			}
+			if((typeof unique1[i]=="number" && typeof unique2[i]=="string") ||(typeof unique1[i]=="string" && typeof unique2[i]=="number")  ){
+				diff++;
+			}
+		}
+		return diff;
+	}
 }
 
 
